de651079e33c3a723478d79c7c21560526cbb7dc
Josh Raiborde <joshsrai@yahoo.com>
2020-09-06 17:23:51 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,14 +1,3 @@
 class Show < ActiveRecord::Base
   
-  def self.highest_rating
-    self.maximum("rating")
-  end
-  
-  def self.most_popular_show
-    self.find_by(rating: self.highest_rating)
-  end
-    
-  
-  
-  
 end
